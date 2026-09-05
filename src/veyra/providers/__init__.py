258b6ac500edb294429c79626a1678906baacccc
Mir Resanul Karim <resanul@gmail.com>
2026-09-05 19:49:53 +0600

    feat: expose network stream resolver

--- src/veyra/providers/__init__.py
+++ src/veyra/providers/__init__.py
@@ -3,5 +3,14 @@
 from .models import SearchResult, StreamSource
 from .registry import ProviderRegistry
 from .catalog import CatalogProvider, CatalogSection
+from .stream_resolver import ManifestInfo, StreamResolver
 
-__all__ = ["ProviderRegistry", "SearchResult", "StreamSource", "CatalogProvider", "CatalogSection"]
+__all__ = [
+    "ProviderRegistry",
+    "SearchResult",
+    "StreamSource",
+    "CatalogProvider",
+    "CatalogSection",
+    "ManifestInfo",
+    "StreamResolver",
+]
