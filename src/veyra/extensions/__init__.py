f9318c1650ea27f07604af0b14b1a234cace1cff
Mir Resanul Karim <resanul@gmail.com>
2026-09-05 13:29:08 +0600

    feat: export CS3 compatibility boundary

--- src/veyra/extensions/__init__.py
+++ src/veyra/extensions/__init__.py
@@ -1,7 +1,17 @@
 """VEYRA extension system."""
 
+from .cs3 import CS3Inspection, CS3Inspector, CS3Manifest, CS3RuntimeAdapter, CS3RuntimeUnavailable
 from .installer import ExtensionInstaller
 from .manifest import ExtensionManifest
 from .manager import ExtensionManager
 
-__all__ = ["ExtensionManifest", "ExtensionManager", "ExtensionInstaller"]
+__all__ = [
+    "ExtensionManifest",
+    "ExtensionManager",
+    "ExtensionInstaller",
+    "CS3Manifest",
+    "CS3Inspection",
+    "CS3Inspector",
+    "CS3RuntimeAdapter",
+    "CS3RuntimeUnavailable",
+]
