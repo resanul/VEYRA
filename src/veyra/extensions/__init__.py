68ac1a8fc7fd2b971887c75d603211e226ad7d07
Mir Resanul Karim <resanul@gmail.com>
2026-09-05 14:00:40 +0600

    feat: export CS3 runtime and CloudStream bridge APIs

--- src/veyra/extensions/__init__.py
+++ src/veyra/extensions/__init__.py
@@ -1,6 +1,7 @@
 """VEYRA extension system."""
 
-from .cs3 import CS3Inspection, CS3Inspector, CS3Manifest, CS3RuntimeAdapter, CS3RuntimeUnavailable
+from .cs3 import CS3Inspection, CS3Inspector, CS3Manifest, CS3Provider, CS3RuntimeAdapter, CS3RuntimeError, CS3RuntimeUnavailable, CS3Sidecar
+from .cs3_api import CloudStreamApiBridge, CS3ExtractorInfo, CS3ProviderInfo, CS3ProviderResponse
 from .installer import ExtensionInstaller
 from .manifest import ExtensionManifest
 from .manager import ExtensionManager
@@ -12,6 +13,13 @@ __all__ = [
     "CS3Manifest",
     "CS3Inspection",
     "CS3Inspector",
+    "CS3Sidecar",
+    "CS3Provider",
     "CS3RuntimeAdapter",
+    "CS3RuntimeError",
     "CS3RuntimeUnavailable",
+    "CloudStreamApiBridge",
+    "CS3ExtractorInfo",
+    "CS3ProviderInfo",
+    "CS3ProviderResponse",
 ]
