0a40fed7399ffbe55d9b48cf165405bbde92fcd4
Mir Resanul Karim <resanul@gmail.com>
2026-09-05 12:14:11 +0600

    feat: expose extension installer

--- src/veyra/extensions/__init__.py
+++ src/veyra/extensions/__init__.py
@@ -1,6 +1,7 @@
 """VEYRA extension system."""
 
+from .installer import ExtensionInstaller
 from .manifest import ExtensionManifest
 from .manager import ExtensionManager
 
-__all__ = ["ExtensionManifest", "ExtensionManager"]
+__all__ = ["ExtensionManifest", "ExtensionManager", "ExtensionInstaller"]
